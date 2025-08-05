3f5372c57a35cae484ec06ce65a702c0f0ca29ea
lucas <lucasaita4000@gmail.com>
2025-08-05 03:57:28 -0300

    initial commit

---
+++ src/main/java/br/com/lucasflix/LucasFlixApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasflix/LucasFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
