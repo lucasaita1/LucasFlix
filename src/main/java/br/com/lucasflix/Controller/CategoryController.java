1b3bd122459b969ae83af8c31167ffa171c949df
lucas <lucasaita4000@gmail.com>
2025-08-06 04:25:03 -0300

    feat: add CategoryController class in project

---
+++ src/main/java/br/com/lucasflix/Controller/CategoryController.java
@@ -0,0 +1,17 @@
+package br.com.lucasflix.Controller;
+
+import br.com.lucasflix.Service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+
+}
