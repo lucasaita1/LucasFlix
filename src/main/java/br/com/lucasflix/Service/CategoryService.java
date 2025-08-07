debe7797fee121599d3fc396aa5e072210e2dc0d
lucas <lucasaita4000@gmail.com>
2025-08-07 04:31:36 -0300

    feat: adds functionality to the CategoryService class

--- src/main/java/br/com/lucasflix/Service/CategoryService.java
+++ src/main/java/br/com/lucasflix/Service/CategoryService.java
@@ -1,7 +1,36 @@
 package br.com.lucasflix.Service;
 
+import br.com.lucasflix.Repository.CategoryRepository;
+import br.com.lucasflix.entity.Category;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category newCategory(Category category) {
+        return repository.save(category);
+    }
+
+    public Optional<Category> categoryFindByID(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deleteCategoryFindByID(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        }
+
+    }
+
 }
