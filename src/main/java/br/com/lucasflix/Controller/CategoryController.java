fb9129165b14502f705ca875e13a992565c4b397
lucas <lucasaita4000@gmail.com>
2025-08-07 04:32:48 -0300

    feat: adds functional routes in CategoryController

--- src/main/java/br/com/lucasflix/Controller/CategoryController.java
+++ src/main/java/br/com/lucasflix/Controller/CategoryController.java
@@ -1,10 +1,12 @@
 package br.com.lucasflix.Controller;
 
 import br.com.lucasflix.Service.CategoryService;
+import br.com.lucasflix.entity.Category;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
@@ -13,5 +15,28 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @GetMapping("/list")
+    public List<Category> getAllCategorys (){
+        return categoryService.findAll();
+    }
+
+    @PostMapping("/create")
+    public Category createCategory (@RequestBody Category category){
+        return categoryService.newCategory(category);
+    }
+
+    @GetMapping("/list/{id}")
+    public Category findByID (@PathVariable Long id){
+        Optional<Category> optionalCategory = categoryService.categoryFindByID(id);
+        if (optionalCategory.isPresent()){
+            return optionalCategory.get();
+        }
+        return null;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteById (@PathVariable Long id){
+        categoryService.deleteCategoryFindByID(id);
+    }
 
 }
