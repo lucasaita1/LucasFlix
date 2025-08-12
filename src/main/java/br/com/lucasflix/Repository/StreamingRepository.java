ec2c70d163dea6e86e39d93b2f3f15092e40e91e
lucas <lucasaita4000@gmail.com>
2025-08-12 02:03:42 -0300

    style: add @Repository annotation in StreamingRepository

--- src/main/java/br/com/lucasflix/Repository/StreamingRepository.java
+++ src/main/java/br/com/lucasflix/Repository/StreamingRepository.java
@@ -2,7 +2,9 @@ package br.com.lucasflix.Repository;
 
 import br.com.lucasflix.entity.Streaming;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamingRepository extends JpaRepository<Streaming, Long> {
 
 
