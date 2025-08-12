f9f9c34be34a0deb47db80bf62cc4d3800ef872b
lucas <lucasaita4000@gmail.com>
2025-08-12 03:30:23 -0300

    fix: add annotations of MovieService class and instance MovieRepository

--- src/main/java/br/com/lucasflix/Service/MovieService.java
+++ src/main/java/br/com/lucasflix/Service/MovieService.java
@@ -1,4 +1,12 @@
 package br.com.lucasflix.Service;
 
+import br.com.lucasflix.Repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+
+    private final MovieRepository repository;
 }
