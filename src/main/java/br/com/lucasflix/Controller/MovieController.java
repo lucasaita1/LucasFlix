fb68147246cbd875920776e3706b7761017fc181
lucas <lucasaita4000@gmail.com>
2025-08-12 03:33:04 -0300

    fix: adds the missing annotations and instantiates the MovieService

--- src/main/java/br/com/lucasflix/Controller/MovieController.java
+++ src/main/java/br/com/lucasflix/Controller/MovieController.java
@@ -1,4 +1,15 @@
 package br.com.lucasflix.Controller;
 
+
+import br.com.lucasflix.Service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+@RequiredArgsConstructor
 public class MovieController {
+
+    private final MovieService service;
 }
