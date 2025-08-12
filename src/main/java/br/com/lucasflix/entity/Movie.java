d020ac9cbe5f5e02bd7085ceb8560c070c9fd572
lucas <lucasaita4000@gmail.com>
2025-08-12 03:27:51 -0300

    feat: adds the attributes of the Movie class and establishes the relationship with the Category and Streaming tables

--- src/main/java/br/com/lucasflix/entity/Movie.java
+++ src/main/java/br/com/lucasflix/entity/Movie.java
@@ -1,4 +1,60 @@
 package br.com.lucasflix.entity;
 
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "movie")
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    private double rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToMany
+            @JoinTable(name = "movie_category",
+                    joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+            )
+    List<Category> categories;
+
+    @ManyToMany
+
+    @JoinTable(name = "movie_streaming",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "streaming_id")
+    )
+    List<Streaming> streamings;
+
 }
