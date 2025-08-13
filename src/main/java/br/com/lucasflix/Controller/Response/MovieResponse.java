2fcb64d9a4e5117584b92bec099287e6c3c877d8
lucas <lucasaita4000@gmail.com>
2025-08-13 05:11:17 -0300

    fix: Fixes the MovieResponse by adding the correct attributes and making it functional.

--- src/main/java/br/com/lucasflix/Controller/Response/MovieResponse.java
+++ src/main/java/br/com/lucasflix/Controller/Response/MovieResponse.java
@@ -1,4 +1,16 @@
 package br.com.lucasflix.Controller.Response;
 
-public record MovieResponse(Long id, String name) {
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record MovieResponse(Long id,
+                            String title,
+                            String description,
+                            LocalDate releaseDate,
+                            double rating,
+                            List<CategoryResponse> categories,
+                            List<StreamingResponse> streamings) {
 }
