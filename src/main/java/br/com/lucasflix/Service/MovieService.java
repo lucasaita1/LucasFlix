74fd443f1120e1bbd113ba48e23eeff386cf44ad
lucas <lucasaita4000@gmail.com>
2025-08-13 05:05:16 -0300

    fix: fixes empty MovieService by putting two methods: findAll and Save entity

--- src/main/java/br/com/lucasflix/Service/MovieService.java
+++ src/main/java/br/com/lucasflix/Service/MovieService.java
@@ -1,12 +1,23 @@
 package br.com.lucasflix.Service;
 
 import br.com.lucasflix.Repository.MovieRepository;
+import br.com.lucasflix.entity.Movie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository repository;
+
+    public Movie save (Movie movie){
+        return repository.save(movie);
+    }
+
+    public List<Movie> findAll(){
+        return repository.findAll();
+    }
 }
