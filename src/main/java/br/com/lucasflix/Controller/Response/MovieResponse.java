eb5986b299d10164c7696e691a438f5f07e7fae8
lucas <lucasaita4000@gmail.com>
2025-08-14 03:21:34 -0300

    fix: fixes MovieResponse by adding the @JsonFormat annotation to work with the Brazilian date model

--- src/main/java/br/com/lucasflix/Controller/Response/MovieResponse.java
+++ src/main/java/br/com/lucasflix/Controller/Response/MovieResponse.java
@@ -1,5 +1,6 @@
 package br.com.lucasflix.Controller.Response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.util.List;
 public record MovieResponse(Long id,
                             String title,
                             String description,
+                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                             LocalDate releaseDate,
                             double rating,
                             List<CategoryResponse> categories,
