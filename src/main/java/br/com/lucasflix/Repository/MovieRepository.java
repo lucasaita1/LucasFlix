7d490fe6b61f4372969e920e967de70263f8a43e
lucas <lucasaita4000@gmail.com>
2025-08-15 04:40:25 -0300

    feat: adds new method to list movies based on categories

--- src/main/java/br/com/lucasflix/Repository/MovieRepository.java
+++ src/main/java/br/com/lucasflix/Repository/MovieRepository.java
@@ -1,9 +1,14 @@
 package br.com.lucasflix.Repository;
 
+import br.com.lucasflix.entity.Category;
 import br.com.lucasflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByCategories (List<Category> categories);
 }
