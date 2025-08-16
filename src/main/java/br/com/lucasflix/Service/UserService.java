54505e238e32306f0eba8528133d43b8f38b5076
lucas <lucasaita4000@gmail.com>
2025-08-16 18:19:48 -0300

    chore: add UserService class to be configured soon

---
+++ src/main/java/br/com/lucasflix/Service/UserService.java
@@ -0,0 +1,12 @@
+package br.com.lucasflix.Service;
+
+import br.com.lucasflix.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+}
