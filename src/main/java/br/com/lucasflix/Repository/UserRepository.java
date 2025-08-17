1afa1b36f38f89ab14b40b8a0b7b7eb51ff8fbed
lucas <lucasaita4000@gmail.com>
2025-08-17 06:17:23 -0300

    fixes instancing errors of the User class

--- src/main/java/br/com/lucasflix/Repository/UserRepository.java
+++ src/main/java/br/com/lucasflix/Repository/UserRepository.java
@@ -1,8 +1,9 @@
 package br.com.lucasflix.Repository;
 
+import br.com.lucasflix.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
