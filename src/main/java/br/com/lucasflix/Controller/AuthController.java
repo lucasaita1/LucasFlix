0b14dfa0d518a1d4e81c63907518da023d2df6e5
lucas <lucasaita4000@gmail.com>
2025-08-18 05:13:56 -0300

    fix: fixes error generated by the lack of the @RequestBody annotation in the body of the Post method

--- src/main/java/br/com/lucasflix/Controller/AuthController.java
+++ src/main/java/br/com/lucasflix/Controller/AuthController.java
@@ -9,18 +9,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService service;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> saveUser(UserRequest request){
+    public ResponseEntity<UserResponse> saveUser(@RequestBody UserRequest request){
         User user = UserMapper.toUser(request);
         User newUser = service.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED)
