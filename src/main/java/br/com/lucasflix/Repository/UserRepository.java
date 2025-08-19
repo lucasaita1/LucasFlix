1ab8adefdc8dc295637551cf8c014f057596f799
lucas <lucasaita4000@gmail.com>
2025-08-19 04:59:20 -0300

    feat: creates through JPA a findUserByEmail to be used in the SecurityAuthService to help validate the user's password with the email registered in the database

--- src/main/java/br/com/lucasflix/Repository/UserRepository.java
+++ src/main/java/br/com/lucasflix/Repository/UserRepository.java
@@ -2,8 +2,13 @@ package br.com.lucasflix.Repository;
 
 import br.com.lucasflix.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findUserByEmail (String email);
 }
