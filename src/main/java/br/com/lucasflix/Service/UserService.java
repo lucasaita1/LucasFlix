fa40c4ddd35f699dbced0ad43b164917e8f1e781
lucas <lucasaita4000@gmail.com>
2025-08-19 05:03:34 -0300

    feat: brings the passwordencoder interface to encode user passwords so they are not exposed in the database or in requests

--- src/main/java/br/com/lucasflix/Service/UserService.java
+++ src/main/java/br/com/lucasflix/Service/UserService.java
@@ -3,6 +3,7 @@ package br.com.lucasflix.Service;
 import br.com.lucasflix.Repository.UserRepository;
 import br.com.lucasflix.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
    public User saveUser(User user){
+       String password = user.getPassword();
+       user.setPassword(passwordEncoder.encode(password));
        return repository.save(user);
    }
 }
