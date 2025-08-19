b054720976d223d8ec07668c44aba09a4d5661a4
lucas <lucasaita4000@gmail.com>
2025-08-19 04:56:18 -0300

    feat: Users implements interface UserDetails for to use Spring Security

--- src/main/java/br/com/lucasflix/entity/User.java
+++ src/main/java/br/com/lucasflix/entity/User.java
@@ -2,6 +2,11 @@ package br.com.lucasflix.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -10,7 +15,7 @@ import lombok.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +26,35 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
