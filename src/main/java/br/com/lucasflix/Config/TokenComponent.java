3478b8efc8b872385993c6822364a58477a34303
lucas <lucasaita4000@gmail.com>
2025-08-20 05:15:14 -0300

    feat: through JWTDataUser creates a verification method for the provided Token.

--- src/main/java/br/com/lucasflix/Config/TokenComponent.java
+++ src/main/java/br/com/lucasflix/Config/TokenComponent.java
@@ -3,10 +3,14 @@ package br.com.lucasflix.Config;
 import br.com.lucasflix.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Component;
 
+
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenComponent {
@@ -22,8 +26,28 @@ public class TokenComponent {
         return JWT.create()
                 .withSubject(user.getEmail())
                 .withClaim("userId", user.getId())
+                .withClaim("name", user.getName())
                 .withExpiresAt(Instant.now().plusSeconds(86400))
                 .withIssuedAt(Instant.now())
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken (String token){
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT jwt = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(jwt.getClaim("userId").asLong())
+                    .name(jwt.getClaim("name").asString())
+                    .email(jwt.getSubject())
+                    .build());
+        } catch (JWTVerificationException exception) {
+            return Optional.empty();
+        }
     }
 }
