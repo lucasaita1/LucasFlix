20f1c5b30e609d791f4b3f6efacde47ca99535e2
lucas <lucasaita4000@gmail.com>
2025-08-22 03:50:18 -0300

    feat: add method where it creates a list of errors where it sends messages of exceptions handled to this list of errors if there is more than one error

--- src/main/java/br/com/lucasflix/Config/ApplicationControllerAdvice.java
+++ src/main/java/br/com/lucasflix/Config/ApplicationControllerAdvice.java
@@ -3,9 +3,14 @@ package br.com.lucasflix.Config;
 
 import br.com.lucasflix.Exception.UsernameAndPasswordInvalid;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -15,4 +20,19 @@ public class ApplicationControllerAdvice {
     public String handleNotFoundExeception (UsernameAndPasswordInvalid ex){
         return ex.getMessage();
     }
-}
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
+    }
+
+    }
